5de1806cbd7ccbb91a3a741c1172af1368c991be
Wilander Velazco <wilandervelazco@gmail.com>
2022-06-12 21:16:44 -0400

    iniciando la segunda entrega

---
+++ models/productos.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const productosCollection = 'productos'
+
+const productosSchema = new mongoose.Schema({
+    id: {type: Number, required: true},
+    timestamp: {type: String, required: true},
+    nombre: {type: String, required: true},
+    descripcion: {type: String, required: true},
+    codigo: {type: String, required: true},
+    foto: {type: String, required: true},
+    precio: {type: Number, required: true},
+    stock: {type: Number, required: true},
+})
+
+export const productos = mongoose.model(productosCollection, productosSchema)
